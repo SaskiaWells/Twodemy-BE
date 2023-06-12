232d355ab2f471486cbe7dd9f085f73dd1e2735e
BenjiSmith1990 <benmwsmith@gmail.com>
2023-06-12 10:55:45 +0100

    Add article_img to teacher document

--- individual-data/ben.js
+++ individual-data/ben.js
@@ -93,6 +93,7 @@ const userObject = {
             default: () => new ObjectId(),
           },
           article_title: "Mongod Tutorial",
+          article_img: "https://coralogix.com/wp-content/uploads/2023/03/MongoDB-1000X1000.png",
           article_body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
           created_by: "MrTeacher",
           
