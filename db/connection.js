5d18cc99a2b1cee8a948ade788622881e2365114
Martin Abbott <abbott01@googlemail.com>
2023-06-14 14:21:31 +0100

    Added const source declaration to line 5 of connection.js, then replaced createConnection URL with source

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const ENV = process.env.NODE_ENV || "test";
 const dotenv = require("dotenv");
 dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
+const source = process.env.MONGODB_URL;
 
-const connectionPool = mongoose.createConnection(process.env.MONGODB_URL, {
+const connectionPool = mongoose.createConnection(source, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
