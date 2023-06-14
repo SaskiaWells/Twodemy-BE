c7d950cb1e49f130b5d71877abb081560d4206ff
Martin Abbott <abbott01@googlemail.com>
2023-06-14 14:12:39 +0100

    declared dotenv on line 3 of connection.js, then changed line 4 to dotenv.config...

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const ENV = process.env.NODE_ENV || "test";
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+const dotenv = require("dotenv");
+dotenv.config({ path: `${__dirname}/../.env.${ENV}` });
 
 const connectionPool = mongoose.createConnection(process.env.MONGODB_URL, {
 	useNewUrlParser: true,
