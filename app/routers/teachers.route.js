418b677b57951b9903a7f2c5da4567f41a68fde0
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-06-17 11:01:48 +0100

    completed patch teacher

--- app/routers/teachers.route.js
+++ app/routers/teachers.route.js
@@ -1,11 +1,12 @@
 const {
-	getTeachers,
-	getTeacherById,
+  getTeachers,
+  getTeacherById,
+  updateTeacher,
 } = require("../../controllers/teachers.controllers");
 
 const teachersRouter = require("express").Router();
 
 teachersRouter.route("/").get(getTeachers);
-teachersRouter.route("/:_id").get(getTeacherById);
+teachersRouter.route("/:_id").get(getTeacherById).patch(updateTeacher);
 
 module.exports = teachersRouter;
