91856af99cab0a1d974dc7c78f2a6cddab84478e
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-06-17 12:45:01 +0100

    completed delete user

--- app/routers/users.route.js
+++ app/routers/users.route.js
@@ -1,11 +1,12 @@
 const usersRouter = require("express").Router();
-const { getUsers } = require("../../controllers/users.controllers");
+const { getUsers, deleteUser } = require("../../controllers/users.controllers");
 const coursesRouter = require("./courses.route");
 const articleRouter = require("./articles.route");
 const studentsRouter = require("./students.route");
 const teachersRouter = require("./teachers.route");
 
 usersRouter.get("/", getUsers);
+usersRouter.delete("/:_id", deleteUser);
 usersRouter.use("/students", studentsRouter);
 usersRouter.use("/teachers", teachersRouter);
 usersRouter.use("/courses", coursesRouter);
